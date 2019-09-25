fe62ac38d49639dcc937716f4e6bf9b5d44a083f
Dan Tipaldo <dtipaldo@gmail.com>
2019-09-25 08:02:26 -0400

    scrap 30 year rate

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-const axios = require('axios');
+const Nightmare = require('nightmare');
 const cheerio = require('cheerio');
+
+const nightmare = Nightmare({
+    show: false
+    });
 
 const url = 'https://www.rate.com/loan-expert/andrewmarquis'
 
-axios.get(url)
-    .then ( response => {
-        console.log(response.data);
+fixedRate30 = null;
+
+nightmare
+    .goto(url)
+    .wait('.todaysRates-container')
+    .evaluate(() => document.querySelector('.todaysRates-container').innerHTML)
+    .end()
+    .then( response => {
+        console.log(getFixedRate30(response));
     })
-    .catch( error => {
-        console.log(error)
+    .catch(err => {
+        console.log(err);
     });
+
+function getFixedRate30(html) {
+    const $ = cheerio.load(html);
+    return $('table.gr-table').find('tbody tr:nth-child(1)').text();
+}
+
