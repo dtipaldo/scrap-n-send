4ab4efe44190b2184eb0b7709807c3b5881d45e6
Dan Tipaldo <dtipaldo@gmail.com>
2019-09-26 21:25:06 -0400

    fetch 30 year rate from rate.com

--- index.js
+++ index.js
@@ -1,28 +1,60 @@
 const Nightmare = require('nightmare');
 const cheerio = require('cheerio');
+const moment = require('moment');
+// const Date = require('date');
 
-const nightmare = Nightmare({
-    show: false
-    });
+class RateScraper {
+    constructor(URL) {
+        this.previousFixedRate30 = null;
+        this.currentFixedRate30 = null;
+        this.URL = URL;
+        // this.nightmare = nightmare;
+    }
 
-const url = 'https://www.rate.com/loan-expert/andrewmarquis'
+    async fetchAndEmail() {
+        this.currentFixedRate30 = await this.fetchRateAsync();
+        if (this.previousFixedRate30 == this.currentFixedRate30) {
+           console.log("Rate is the same!");
+        }
+        console.log(moment().toString(), "-", this.currentFixedRate30);
+        this.previousFixedRate30 = this.currentFixedRate30;
+    }
 
-fixedRate30 = null;
+    async fetchRateAsync() {
+        const nightmare = Nightmare({show: false});
+        return nightmare
+            .goto(this.URL)
+            .wait('.todaysRates-container')
+            .evaluate(() => {
+                return document.querySelector('body').innerHTML;
+            })
+            .end()
+            .then(response => {
+                return this.parseFixedRate30(response);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
 
-nightmare
-    .goto(url)
-    .wait('.todaysRates-container')
-    .evaluate(() => document.querySelector('.todaysRates-container').innerHTML)
-    .end()
-    .then( response => {
-        console.log(getFixedRate30(response));
-    })
-    .catch(err => {
-        console.log(err);
-    });
-
-function getFixedRate30(html) {
-    const $ = cheerio.load(html);
-    return $('table.gr-table').find('tbody tr:nth-child(1)').text();
+    parseFixedRate30(html) {
+        const $ = cheerio.load(html);
+        const fixedRate30 = $('table.gr-table').find('tbody tr:nth-child(1) td:nth-child(2)').text();
+        return Number(fixedRate30.split('%')[0]);
+    }
 }
 
+const URL = 'https://www.rate.com/loan-expert/andrewmarquis';
+// const nightmare = Nightmare({show: false});
+
+rateScraper = new RateScraper(URL);
+interval = setInterval(() => rateScraper.fetchAndEmail(),10000);
+// interval = setInterval(async function () {
+//     await rateScraper.fetchRateAsync();
+//     if (rateScraper.previousFixedRate30 == rateScraper.currentFixedRate30) {
+//         console.log("Rate is the same!")
+//     }
+//     console.log(moment().toString(), "-", this.currentFixedRate30);
+//     rateScraper.previousFixedRate30 = rateScraper.currentFixedRate30
+//
+// }, 10000 );
